e7541f52c161cc7cc1aebdda69fc520c30bbc7c7
AngelaSheahan <alouise_35@yahoo.co.uk>
2022-10-05 20:18:14 +0000

    add category selection option for player

--- run.py
+++ run.py
@@ -21,98 +21,99 @@ SHEET = GSPREAD_CLIENT.open('Wordle-Hangman')
 
 game_intro = "Welcome to Wordle-Hangman! As the name implies, all words are of 5\
     letters in length. You, the player, selects a Word Category. The game then\
-    randomly selects a word from this category for you to guess. Good Luck!" 
+    randomly selects a word from your chosen category. You then guess what this \
+    letter is; one letter at a time. Good Luck!" 
 
 print(game_intro)  
+
 
 game_rules = " 1. Select one of 6 categories.\
     2. The game will randomly generate a word from your choosen category.\
     3. Enter one letter at a time to try to guess this word.\
-    4. You have 4 attempts."
-    
+    4. You have 4 attempts."\
 
 print(game_rules)
+
+print("Please see list of word categories below:")
 
 # All category headings appear in row 1 of spreadsheet
 words = SHEET.worksheet("wordlist").row_values(1)
 
 # Use list comprehension to convert list of lists from google sheet into list of strings"""
-words_string = [''.join(ele) for ele in words]
+word_categories = [''.join(ele) for ele in words]
 
-choice = input("please select a category from the following list. Enter you choice here: " )
-
-
-# Use random() to generate a random word from the choosen category of words.
-picked = random.choice(SHEET.worksheet.col_values.choice)
-
-print(picked)
-
-print('The word has' , len(picked), 'letters')
-
-correct = ['_'] * len(picked)
-incorrect = []
-
-def update():
-    for x in correct:
-        print(x, end=' ')
-    print()
-
-def wait():
-    for i in range(5):
-        print('.', end ="")
-        sleep(.5)
-    print()
-print("Let me check")
-
-wait()
-
-update()
-bodyParts(len(incorrect))
-
-# function asking player if they wish to play a new game or exit game
-# def new_game_exit():
+print(word_categories)
 
 
-    
-
-while True:
-
-    print('############################################')
-    guess = input("Guess one letter: ")[0]
-
-    if guess.isnumeric():
-        print("Enter a letter not a number: ")
-        continue
-    if guess in picked:
-        index = 0
-        for x in picked:
-            if x == guess:
-                correct[index] = guess
-            index +=1
-        update()
-    else:
-        if guess not in incorrect:
-            incorrect.append(guess)
-            bodyParts(len(incorrect))
-        else:
-            print("You have already guessed that")
-        print(incorrect)
-        if len(incorrect) > 4:
-            print("You lose")
-            print("The word was" , picked)
-            break
-    if '_' not in correct:
-        print("You win")
-        break
+# Handling the player category choice
+choice = 1
+while (choice < 7):
+    choice = int(input("Enter your choice = "))
+    picked = random.choice(SHEET.worksheet.col_values.choice)
+    print(picked)
+    choice = choice + 1
+print("Choose Word Category 1, 2, 3, 4, 5 or 6")
 
 
+ 
 
 
+# # Use random() to generate a random word from the choosen category of words.
+# picked = random.choice(SHEET.worksheet.col_values.choice)
 
+# print(picked)
 
+# print('The word has' , len(picked), 'letters')
 
-        
+# correct = ['_'] * len(picked)
+# incorrect = []
 
+# def update():
+#     for x in correct:
+#         print(x, end=' ')
+#     print()
 
+# def wait():
+#     for i in range(5):
+#         print('.', end ="")
+#         sleep(.5)
+#     print()
+# print("Let me check")
 
+# wait()
 
+# update()
+# bodyParts(len(incorrect))
+
+# # function asking player if they wish to play a new game or exit game
+# # def new_game_exit():
+
+# while True:
+
+#     print('############################################')
+#     guess = input("Guess one letter: ")[0]
+
+#     if guess.isnumeric():
+#         print("Enter a letter not a number: ")
+#         continue
+#     if guess in picked:
+#         index = 0
+#         for x in picked:
+#             if x == guess:
+#                 correct[index] = guess
+#             index +=1
+#         update()
+#     else:
+#         if guess not in incorrect:
+#             incorrect.append(guess)
+#             bodyParts(len(incorrect))
+#         else:
+#             print("You have already guessed that")
+#         print(incorrect)
+#         if len(incorrect) > 4:
+#             print("You lose")
+#             print("The word was" , picked)
+#             break
+#     if '_' not in correct:
+#         print("You win")
+#         break
