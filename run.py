9f0c3ba3840ae4c066c28266dff456682dfc1972
AngelaSheahan <alouise_35@yahoo.co.uk>
2022-10-09 17:24:12 +0000

    put all code in functions

--- run.py
+++ run.py
@@ -22,16 +22,17 @@ SHEET = GSPREAD_CLIENT.open('Wordle-Hangman')
 
 
 # Wordle-Hangman game introduction
-def intro_rules:
-    game_intro = "Welcome to Wordle-Hangman! As the name implies, all words are of 5\
-    letters in length. You, the player, selects a Word Category. The game then\
-    randomly selects a word from your chosen category. You then guess what this \
-    letter is; 1 letter at a time. Good Luck!"
-    game_rules = " 1. Select one of 6 categories.\
-    2. The game will randomly generate a word from your choosen category.\
-    3. Enter one letter at a time to try to guess this word.\
-    4. You have 4 attempts."\
-    print(into_rules)
+# def intro_rules():
+#     game_intro = "Welcome to Wordle-Hangman! As the name implies, all words are of 5\
+#     letters in length. You, the player, selects a Word Category. The game then\
+#     randomly selects a word from your chosen category. You then guess what this \
+#     letter is; 1 letter at a time. Good Luck!"
+#     game_rules = " 1. Select one of 6 categories.\
+#     2. The game will randomly generate a word from your choosen category.\
+#     3. Enter one letter at a time to try to guess this word.\
+#     4. You have 4 attempts."\
+#     print(game_intro)
+#     print(game_rules)
 
 
 # Player selects one of 6 word categories from list of strings pulled from Wordle-Hangman google sheet """
@@ -76,8 +77,8 @@ def new_game_exit():
     playagain = str(input("Do you want to play again? [y] / [n]: "))
     if playagain == 'y':
         initialise_game()
+        choose_category()
        
-  
     else:
         print("Goodbye!")
 
@@ -94,18 +95,20 @@ def play_game():
         
         print('############################################')
         guess = input("Guess one letter: ")[0]
-
-    if guess.isnumeric():
+        
+        if guess.isnumeric():
             print("Enter a letter not a number: ")
             continue
-    if guess in random_word:
+        
+        if guess in random_word:
             index = 0
             for x in random_word:
                 if x == guess:
                     correct[index] = guess
                 index +=1
             update()
-    else:
+            
+        else:
             if guess not in incorrect:
                 incorrect.append(guess)
                 bodyParts(len(incorrect))
@@ -117,22 +120,29 @@ def play_game():
                 print("The word was" , random_word)
             
                 new_game_exit()
-
-           
-
-    if '_' not in correct:
+                
+        if '_' not in correct:
+            
             print("You win")
             
             new_game_exit()
 
 
 
-# Funtion Calls          
-initialise_game()
+# Funtion Calls 
 
-wait()
+# intro_rules()
+
+categories()
+
+choose_category()
 
 update()
 
-bodyParts(len(incorrect))
+wait()
+
+play_game()
+
+initialise_game()
+
 
