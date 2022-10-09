89043e4c6247c210b36d406f9ae565465ac9c4d8
AngelaSheahan <alouise_35@yahoo.co.uk>
2022-10-09 21:06:39 +0000

    add new game and exit game function

--- run.py
+++ run.py
@@ -1,11 +1,7 @@
-# Your code goes here.
-# You can delete these comments, but do not change the name of this file
-# Write your code to expect a terminal of 80 characters wide and 24 rows high
 import gspread
 import random
 from hangman_parts import bodyParts
 from time import sleep
-
 from google.oauth2.service_account import Credentials
 
 SCOPE = [
@@ -13,50 +9,60 @@ SCOPE = [
     "https://www.googleapis.com/auth/drive.file",
     "https://www.googleapis.com/auth/drive"
     ]
-
 CREDS = Credentials.from_service_account_file('creds.json')
 SCOPED_CREDS = CREDS.with_scopes(SCOPE)
 GSPREAD_CLIENT = gspread.authorize(SCOPED_CREDS)
 SHEET = GSPREAD_CLIENT.open('Wordle-Hangman')
-
+wks = SHEET.worksheet("wordlist")
+correct = []
+incorrect = []
+word_categories = []
+choice = 0
+random_word = ""
 
 
 # Wordle-Hangman game introduction
-# def intro_rules():
-#     game_intro = "Welcome to Wordle-Hangman! As the name implies, all words are of 5\
-#     letters in length. You, the player, selects a Word Category. The game then\
-#     randomly selects a word from your chosen category. You then guess what this \
-#     letter is; 1 letter at a time. Good Luck!"
-#     game_rules = " 1. Select one of 6 categories.\
-#     2. The game will randomly generate a word from your choosen category.\
-#     3. Enter one letter at a time to try to guess this word.\
-#     4. You have 4 attempts."\
-#     print(game_intro)
-#     print(game_rules)
+def information():
+    game_intro = "Welcome to Wordle-Hangman! As the name implies, all words are of 5\
+    letters in length. You, the player, selects a Word Category. The game then\
+    randomly selects a word from your chosen category. You then guess what this \
+    letter is; 1 letter at a time. Good Luck!"
+    game_rules = " 1. Select one of 6 categories.\
+    2. The game will randomly generate a word from your choosen category.\
+    3. Enter one letter at a time to try to guess this word.\
+    4. You have 4 attempts."
+    print(game_intro)
+    print(game_rules)
 
 
 # Player selects one of 6 word categories from list of strings pulled from Wordle-Hangman google sheet """
-def categories():
-    print("Please see list of word categories below:")
+def load_categories():
+    global word_categories
     # All category headings appear in row 1 of spreadsheet
-    wks = SHEET.worksheet("wordlist")
     words = wks.row_values(1)
     # Use list comprehension to convert list of lists from google sheet into list of strings"""
     word_categories = [''.join(ele) for ele in words]
-    print(word_categories)
 
 
 def choose_category():
+    global choice
+    print("Please see list of word categories below:")
+    print(word_categories)
     choice = int(input("Enter your choice = "))
     while (choice < 1 or choice >= 7):
         print("This is not a valid category number")
         choice = int(input("Enter your choice = "))
     category = word_categories[choice - 1]
-    picked = SHEET.worksheet("wordlist").find(category).value
+    picked = wks.find(category).value
     print("Your choice is: ",(picked))
+
+
+def select_random_word():
+    global random_word
     word_list = wks.col_values(choice)
     random_word = random.choice(word_list[1:])
-    return (random_word)
+    print('The word has' , len(random_word), 'letters')
+
 
 # Update correct array with correct answers
 def update():
@@ -72,21 +78,13 @@ def wait():
     print()
 
 
-# Function to enable player the option of exiting the game or playing a new game
-def new_game_exit():
-    playagain = str(input("Do you want to play again? [y] / [n]: "))
-    if playagain == 'y':
-        initialise_game()
-        choose_category()
-       
-    else:
-        print("Goodbye!")
-
-
 # Prepare correct and incorrect variables
 def initialise_game():
-    random_word = choose_category()
-    print('The word has' , len(random_word), 'letters')
+    global correct
+    global incorrect
+    information()
+    choose_category()
+    select_random_word()
     correct = ['_'] * len(random_word)
     incorrect = []
 
@@ -99,13 +97,13 @@ def play_game():
         if guess.isnumeric():
             print("Enter a letter not a number: ")
             continue
-        
+
         if guess in random_word:
             index = 0
             for x in random_word:
                 if x == guess:
                     correct[index] = guess
-                index +=1
+                index += 1
             update()
             
         else:
@@ -118,31 +116,18 @@ def play_game():
             if len(incorrect) > 4:
                 print("You lose!")
                 print("The word was" , random_word)
-            
-                new_game_exit()
+                break
                 
         if '_' not in correct:
-            
             print("You win")
-            
-            new_game_exit()
+            break
 
 
-
-# Funtion Calls 
-
-# intro_rules()
-
-categories()
-
-choose_category()
-
-update()
-
-wait()
-
-play_game()
-
-initialise_game()
-
-
+load_categories()
+while True:
+    initialise_game()
+    play_game()
+    replay = str(input("Do you want to play again? [y] / [n]: "))
+    if replay == 'n':
+        print("Goodbye!")
+        break
